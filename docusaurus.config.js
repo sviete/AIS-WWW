f7f4ab0f0391577f49b78bff97ff5ba13316b4bf
rysiekstation <rysiekstation@gmail.com>
2020-07-25 21:30:15 +0200

    pwa start

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,6 +11,15 @@ module.exports = {
   favicon: 'img/favicon.ico',
   plugins: [
     [
+     '@docusaurus/plugin-ideal-image',
+     {
+       quality: 70,
+       max: 1030, // max resized image's size.
+       min: 640, // min resized image's size. if original is lower, use that size.
+       steps: 2, // the max number of images generated between min and max (inclusive)
+     },
+    ],
+    [
       '@docusaurus/plugin-pwa',
       {
         debug: true,
